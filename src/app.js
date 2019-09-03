22c24e79679f4c058f16e277155be3c802dd410a
Jim Carroll <jim@pulamusic.com>
2019-09-03 15:05:51 -0400

    readied app for deployment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./../utils/geocode')
 const forecast = require('./../utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -100,7 +101,7 @@ app.get('*', (req, res) => {
   })
 })
 
-// run the server
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+// run the development server
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
 })
