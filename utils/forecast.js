f01a2a9db9f2bf8a4087f2bae293333af7dafaa8
Jim Carroll <jim@pulamusic.com>
2019-09-04 08:54:38 -0400

    updated forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -8,13 +8,15 @@ const forecast = (latitude, longitude, callback) => {
   request({url: weatherURL, json: true}, (error, { body }) => {
     const temperature = body.currently.temperature
     const precipitation = body.currently.precipProbability * 100
+    const tempHigh = body.daily.data[0].temperatureHigh
+    const tempLow = body.daily.data[0].temperatureLow
 
     if (error) {
       callback('Unable to connect to the weather service.', undefined)
     } else if (body.error) {
       callback('Unable to find that location. Please try another search.', undefined)
     } else {
-      callback(undefined, `${body.daily.data[0].summary} It is currently ${temperature} degrees out. There is a ${precipitation}% chance of precipitation.`)
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${temperature} degrees out. The high temperature today will be ${tempHigh} degrees, and the low temperature will be ${tempLow} degrees. There is a ${precipitation}% chance of precipitation.`)
     }
   })
 }
